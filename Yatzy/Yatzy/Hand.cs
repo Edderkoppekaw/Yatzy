d8fdba50c90ecf23b24ad27e446a3713d46d0113
Kasper Holst Hansen <khha18@student.aau.dk>
2020-04-25 13:57:19 +0200

    Ændringer Hand.cs

--- Yatzy/Yatzy/Hand.cs
+++ Yatzy/Yatzy/Hand.cs
@@ -10,83 +10,86 @@ namespace Yatzy
     {
         public bool FirstRound { get; private set; } = true; // set this to false when the user has made the first move
         // this class has a hand of all the dices in the game (o)
-        private readonly Terning[] Terninger = 
+        public static void CurrentDice()
         {
-            new Terning(),
-            new Terning(),
-            new Terning(),
-            new Terning(),
-            new Terning(),
-            new Terning()
-        };
+            var diceList = new List<Terning>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                diceList.Add(new Terning());
+            }
+
+        }
+
 
         // Now comes the tricky part, I need a new method where the user chooses a set number of dices which then I have to keep and reroll the others. 
 
-        public void KeepDices(int[] toReroll)
-        {
-
-            // now we need to go through every dice in the toReroll array and check if it's in Dices array. If it's in the array do nothing with it. If we find a value that is not equal to the toReroll number we have to reroll it
-
-
-
-            for (int i = 0; i < Terninger.Length; i++)
+        public void KeepDices(List<Terning> diceList)
             {
-               
 
-                foreach (int roll in toReroll)
+                // now we need to go through every dice in the toReroll array and check if it's in Dices array. If it's in the array do nothing with it. If we find a value that is not equal to the toReroll number we have to reroll it
+
+
+
+                for (int i = 0; i < diceList.Count; i++)
                 {
-                    if (roll == 1)
+
+
+                    foreach (int roll in diceList)
                     {
+                        if (roll == 1)
+                        {
 
-                        Terninger[0].Reroll();
+                            diceList[0].Reroll();
 
+                        }
+
+                        if (roll == 2)
+                        {
+
+                            diceList[1].Reroll();
+
+                        }
+
+                        if (roll == 3)
+                        {
+
+                            diceList[2].Reroll();
+
+                        }
+
+                        if (roll == 4)
+                        {
+
+                            diceList[3].Reroll();
+
+                        }
+
+                        if (roll == 5)
+                        {
+
+                            diceList[4].Reroll();
+
+                        }
+
+                        if (roll == 6)
+                        {
+
+                            diceList[5].Reroll();
+
+                        }
                     }
 
-                    if (roll == 2)
-                    {
 
-                        Terninger[1].Reroll();
 
-                    }
 
-                    if (roll == 3)
-                    {
+                
 
-                        Terninger[2].Reroll();
+                ShowDices();
 
-                    }
-
-                    if (roll == 4)
-                    {
-
-                        Terninger[3].Reroll();
-
-                    }
-
-                    if (roll == 5)
-                    {
-
-                        Terninger[4].Reroll();
-
-                    }
-
-                    if (roll == 6)
-                    {
-
-                        Terninger[5].Reroll();
-
-                    }
-                }
-
-              
 
 
             }
-
-            ShowDices();
-
-
-
         }
 
 
